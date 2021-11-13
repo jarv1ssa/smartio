1c77e5da53523944fc5d8e8c5da4bb2ff126e164
Emin Aliyev <emin.aliyev@stu.khas.edu.tr>
2021-11-13 20:39:11 +0300

    🎉 initialize the project with chakra, router, & firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+import { ColorModeScript } from "@chakra-ui/color-mode";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
